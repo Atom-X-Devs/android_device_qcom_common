121801fd636389fa1d70dcfa55286d1415ccc453
Bharat Pawar <bpawar@codeaurora.org>
2020-09-15 10:49:53 +0530

    Enable servicetracker V1.2 for legacy targets
    Change-Id: Iafb9b12ef22dc779fd45f0d484dd92340670b010

--- base.mk
+++ base.mk
@@ -886,6 +886,11 @@ else
     DELAUN := Launcher3
 endif
 
+#servicetracker HAL
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-impl \
+    vendor.qti.hardware.servicetracker@1.2-service
+
 PRODUCT_PACKAGES += $(ALSA_HARDWARE)
 PRODUCT_PACKAGES += $(ALSA_UCM)
 PRODUCT_PACKAGES += $(ANGLE)
